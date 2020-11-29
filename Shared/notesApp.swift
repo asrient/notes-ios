df9f5857ce3d0e7398a9c20323a2885711993e3e
asrient <asen182000@hotmail.com>
2020-11-29 17:34:15 +0530

    Initial Commit

---
+++ Shared/notesApp.swift
@@ -0,0 +1,17 @@
+//
+//  notesApp.swift
+//  Shared
+//
+//  Created by Aritra Sen on 29/11/20.
+//
+
+import SwiftUI
+
+@main
+struct notesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
